5975d055ecfcd6a9740902ae3dcc3ed817aeccce
Shadow4546 <89007456+Shadow4546@users.noreply.github.com>
2025-02-22 11:33:04 +0530

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_NAME = "Assurexvk_bot"
-API_ID    = 28935416
+API_ID    = "28935416"
 API_HASH  = "e18c05697d95edfe39d8957f6e110308"
 BOT_TOKEN = "7823447094:AAFQIXNDKgLGe-JIaUDHvo0c9fbGv8JwQ8s"
 #MONGO_URI = "mongodb+srv://psrathore1155:<db_password>@cluster0.rpaat.mongodb.net/"

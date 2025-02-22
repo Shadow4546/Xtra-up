293f60a136944ab958d8b97743a6afd4a428e25d
Shadow4546 <89007456+Shadow4546@users.noreply.github.com>
2025-02-22 11:18:39 +0530

    config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ API_ID    = 28935416
 API_HASH  = "e18c05697d95edfe39d8957f6e110308"
 BOT_TOKEN = "7823447094:AAFQIXNDKgLGe-JIaUDHvo0c9fbGv8JwQ8s"
 #MONGO_URI = "mongodb+srv://psrathore1155:<db_password>@cluster0.rpaat.mongodb.net/"
-MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
+MONGO_URI = "mongodb+srv://ankitpatni95:ankitpatni95@cluster0.jberr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
